de8adfadb0b1334c7f172b0376f53415e4bfa3fb
Abdul Hamid <hamidkhattak469@gmail.com>
2024-03-08 02:53:36 +0500

    Services Section completed

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import About from "../../components/About";
 import Hero from "../../components/Hero";
 import Projects from "../../components/Projects";
+import Services from "../../components/Services";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         <Hero />
         <About />
         <Projects />
+        <Services />
     </div>
   )
 }
